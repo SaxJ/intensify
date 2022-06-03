9341ebb9c017f1c1fb15a8b61df5b37b093c716c
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-06-03 16:32:58 +0800

    Make script clean up. Probably won't do this in bash

--- shake.sh
+++ shake.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 image_file=$1
-for ((i=1; i<=50; i++))
+for ((i=1; i<=5; i++))
 do
-    x=$(shuf -i 10-200 -n 1)
-    y=$(shuf -i 10-200 -n 1)
+    x=$(shuf -i 10-100 -n 1)
+    y=$(shuf -i 10-100 -n 1)
     convert "$image_file" -page +"$x"+"$y" -background none -flatten "${i}${image_file}"
-    convert -background white -alpha remove -layers OptimizePlus -delay 25x100 "*${image_file}" -loop 0 output.gif
+done
+convert -background white -alpha remove -layers OptimizePlus -delay 1x300 "*${image_file}" -loop 0 output.gif
+
+for ((i=1; i<=5; i++))
+do
+    rm "${i}${image_file}"
 done
